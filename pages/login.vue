f33a4f4358b71961a2fae33a9c00bebf15658572
Mathisgdi <mathis.ghidotti@epitech.digital>
2023-12-08 11:23:14 +0100

    [UPDATE]Login, register

--- pages/login.vue
+++ pages/login.vue
@@ -6,53 +6,112 @@ const password = ref(null);
 const errorMsg = ref(null);
 
 async function signIn() {
-    try{
-        const {error} = await client.auth.signInWithPassword({
-            email: email.value,
-            password: password.value
-        })
-        if(error){
-            throw error
-        }
-        else{
-            router.push('/')
-        }
+  try {
+    const { error } = await client.auth.signInWithPassword({
+      email: email.value,
+      password: password.value,
+    });
+    if (error) {
+      throw error;
+    } else {
+      router.push("/");
     }
-    catch(error){
-        errorMsg.value = error.message
-    }
+  } catch (error) {
+    errorMsg.value = error.message;
+  }
 }
 </script>
-
 <template>
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-        <div class="bg-white p-8 rounded  shadow-md w150 h-auto">
-            <h2 class="text-2xl font-bold mb-4">Login</h2>
-            <form @submit.prevent="signIn">
-                <div class="mb-4">
-                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
-                        Email
-                    </label>
-                    <input type="email" id="email" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" placeholder="Your email address" v-model="email">
-                </div>
-                <div class="mb-4">
-                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
-                        Password
-                    </label>
-                    <input type="password" id="password" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500" placeholder="Your password" v-model="password">
-                </div>
-                <div>
-                    <p v-if="errorMsg" class="text-red-500 text-xs italic">{{ errorMsg }}</p>
-                </div>
-                <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">SignIn</button>
-                <div>
-                    <p class="text-sm text-gray-500 mt-4">
-                        Don't have an account? 
-                        <NuxtLink to="/register" class="text-blue-500 hover:text-blue-700 text-xs italic">SignUp</NuxtLink>
-                    </p>
-                </div>
-            </form>
-        </div>
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <!-- <NuxtImg src="/img/SneakerLogo.avif" placeholder="logo" /> -->
+      <NuxtLink to="/">
+        <img
+          class="mx-auto h-20 w-auto"
+          src="../public/img/SneakerLogo.avif"
+          alt="Workflow"
+        />
+        <h2
+          class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+        >
+          Sign in to your account
+        </h2>
+      </NuxtLink>
     </div>
-</template>
 
+    <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form @submit.prevent="signIn" class="space-y-6" action="#" method="POST">
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: pl-3 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Email address"
+              v-model="email"
+            />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-sm font-medium leading-6 text-gray-900"
+              >Password</label
+            >
+            <div class="text-sm">
+              <a
+                href="#"
+                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                >Forgot password?</a
+              >
+            </div>
+          </div>
+          <div class="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: pl-3 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Password"
+              v-model="password"
+            />
+          </div>
+        </div>
+        <div>
+          <p v-if="errorMsg" class="text-red-500 text-xs italic">
+            {{ errorMsg }}
+          </p>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+
+      <p class="mt-5 text-center text-sm text-gray-500">
+        Don't have an account ?
+        <NuxtLink
+          to="/register"
+          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >SignUp</NuxtLink
+        >
+      </p>
+    </div>
+  </div>
+</template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,81 +1,110 @@
 <script setup>
-const client = useSupabaseClient()
+const client = useSupabaseClient();
 const email = ref("");
-const password = ref (null);
-const errorMsg = ref (null);
+const password = ref(null);
+const errorMsg = ref(null);
 const successMsg = ref(null);
 
 async function signUp() {
-    console.log('signUp function called'); 
-    try{
-        const {data, error} = await client.auth.signUp({
-            email: email.value,
-            password: password.value
-        })
-        if(error){
-            throw error 
-        }
-        else{
-            successMsg.value = "Check your email to confirm your account"
-        } 
+  console.log("signUp function called");
+  try {
+    const { data, error } = await client.auth.signUp({
+      email: email.value,
+      password: password.value,
+    });
+    if (error) {
+      throw error;
+    } else {
+      successMsg.value = "Check your email to confirm your account";
     }
-    catch(error){
-        errorMsg.value = error.message
-    }
-
+  } catch (error) {
+    errorMsg.value = error.message;
+  }
 }
-
-
 </script>
 
 <template>
-    <div class="flex justify-center items-center h-screen">
-        <form @submit.prevent="signUp" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                    Email
-                </label>
-                <input
-                    class="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    type="email"
-                    placeholder="Enter your email"
-                    v-model="email"
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                    Password
-                </label>
-                <input
-                    class="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password"
-                    type="password"
-                    placeholder="Enter your password"
-                    v-model="password"
-                />
-            </div>
-            <div class="error">
-                <p v-if="errorMsg" class="text-red-500 text-xs italic">{{ errorMsg }}</p>
-            </div>
-            <div>
-                <p v-if="successMsg" class="text-green-500 text-xs italic">{{ successMsg }}</p>
-            </div>
-            <div class="flex items-center justify-between">
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit"
-                >
-                    SignUp
-                </button>
-            </div>
-            <div>
-                <p class="text-gray-700 text-xs mt-3">Already have an account?
-                <NuxtLink to="/login" class="text-blue-500 hover:text-blue-700 text-xs italic">Login</NuxtLink>
-            </p>
-            </div>
-            
-        </form>
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <NuxtLink to="/">
+        <img
+          class="mx-auto h-20 w-auto"
+          src="../public/img/SneakerLogo.avif"
+          alt="Workflow"
+        />
+        <h2
+          class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+        >
+          SignUp
+        </h2>
+      </NuxtLink>
     </div>
-</template>
 
+    <div class="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form @submit.prevent="signUp" class="space-y-6" action="#" method="POST">
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: pl-3 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Email address"
+              v-model="email"
+            />
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-sm font-medium leading-6 text-gray-900"
+              >Password</label
+            >
+          </div>
+          <div class="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: pl-3 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Password"
+              v-model="password"
+            />
+          </div>
+        </div>
+        <div>
+          <p v-if="errorMsg" class="text-red-500 text-xs italic">
+            {{ errorMsg }}
+          </p>
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            SignUp
+          </button>
+        </div>
+      </form>
+
+      <p class="mt-5 text-center text-sm text-gray-500">
+        Already have an account?
+        <NuxtLink
+          to="/login"
+          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >Login</NuxtLink
+        >
+      </p>
+    </div>
+  </div>
+</template>
